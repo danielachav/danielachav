38c38c61278a0224df173b75b282ab40fc5fad3f
Daniela Chavez <danielachavez18@gmail.com>
2019-10-16 16:18:24 -0500

    i really am going all out with this. started the javascript part with the buttons. more to come

--- extra.js
+++ extra.js
@@ -1,5 +1,21 @@
+// THIS IS THE START OF THE BUTTON SECTION //
 
+// THIS IS FOR THE "VIEW MORE" BUTTON ON INDEX HTML UNDER PORTFOLIO SECTION //
 
+$(document).ready(function(){
+    $(".btn btn-dark").click(function(){
+      $(this).button('toggle');
+    });   
+  });
 
+// THIS IS FOR THE BUTTONS ON THE ACTUAL PORTFOLIO PAGE THAT LINK TO GITHUB //
+
+$(document).ready(function(){
+    $(".btn btn-secondary").click(function(){
+      $(this).button('toggle');
+    });   
+  });
+
+// THIS IS THE SCROLLPY SECTION FOR THE INDEX.HTML MAIN PAGE // 
 
 
