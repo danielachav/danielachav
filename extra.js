22eb4aa1b5b8e836df24a50dcb04b6bfc8570382
Daniela Chavez <danielachavez18@gmail.com>
2019-10-16 16:33:28 -0500

    meeeh just some tiny text things

--- extra.js
+++ extra.js
@@ -16,6 +16,39 @@ $(document).ready(function(){
     });   
   });
 
+
+
 // THIS IS THE SCROLLPY SECTION FOR THE INDEX.HTML MAIN PAGE // 
+
+$(document).ready(function() {
+
+    $('body').scrollspy({target: ".navbar", offset: 50});
+
+
+    $("#myNavbar a").on('click', function(event) {
+    
+      
+      if (this.hash !== "") {
+    
+        
+        event.preventDefault();
+    
+        
+        var hash = this.hash;
+    
+        
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+    
+        
+          window.location.hash = hash;
+        });
+    
+      } 
+    
+    });
+
+});
 
 
